ade7d1d8a7eeef757a1bc14ad391be2e6b7d2b1f
chengluyu <chengluyu@live.cn>
2018-10-08 01:17:14 +0800

    add minimizer for CSS files
    Signed-off-by: chengluyu <chengluyu@live.cn>

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,14 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
     minimizer: [
-      new UglifyJsPlugin()
+      new UglifyJsPlugin(),
+      new OptimizeCSSAssetsPlugin()
     ]
   }
 })
